ff20886e6ae2f7d027d125531b7b4dcc87aa0344
Anant1202 <anantsingh1202@gmail.com>
2022-05-26 15:49:40 +0530

    Welcome to Stack and Queue Program

---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StackAndQueue
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue Program");
+        }
+    }
+}
+
