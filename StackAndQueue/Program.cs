4f3819c570fe9fe0c26f7148894756d040dcb59b
Anant1202 <anantsingh1202@gmail.com>
2022-05-26 16:24:45 +0530

    Added:Stack Push

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -7,6 +7,11 @@ namespace StackAndQueue
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stack and Queue Program");
+            Stack obj = new Stack();
+            obj.Push(70);
+            obj.Push(30);
+            obj.Push(56);
+            obj.Display();
         }
     }
 }
---
+++ StackAndQueue/Stack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    //Stack follows LIFO(Last In First Out)
+    public class Stack
+    {
+        //Creating a node top
+        public Node top;
+        //Push will add value 
+        public void Push(int data)
+        {
+            Node new_node = new Node(data);
+            if (top != null)
+            {
+                new_node.next = top;
+            }
+            top = new_node;
+            Console.WriteLine("New Node {0} is added ", new_node.data);
+        }
+        //For Printing or Display of Result
+        public void Display()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Node temp = top;
+            while (temp != null)
+            {
+                Console.WriteLine("|" + temp.data + "|");
+                temp = temp.next;
+            }
+        }
+    }
+}    
+    
